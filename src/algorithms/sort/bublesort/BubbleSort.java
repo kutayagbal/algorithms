9c01cbf9c6deb9f6b25ef36c70d680b8b52fac8b
Kutay Agbal <kutayagbal@gmail.com>
2019-11-10 22:52:32 +0300

    selection sort

--- src/algorithms/sort/bublesort/BubbleSort.java
+++ src/algorithms/sort/bublesort/BubbleSort.java
@@ -1,5 +1,11 @@
 package algorithms.sort.bublesort;
 
+/*
+ * Start at the beginning of the array and swap the first two elements if the
+ * first is greater than the second. Then, go to the next pair, and so on,
+ * continuously making sweeps of the array until it is sorted. In doing so, the
+ * smaller items slowly "bubble" up to the beginning of the list.
+ */
 public class BubbleSort {
 
 	public static void main(String[] args) {
@@ -13,7 +19,7 @@ public class BubbleSort {
 		}
 	}
 
-	private static void myBubbleSort(int[] arr) {
+	static void myBubbleSort(int[] arr) {
 		int index = 0;
 		int len = arr.length;
 		while (index + 1 < len) {
